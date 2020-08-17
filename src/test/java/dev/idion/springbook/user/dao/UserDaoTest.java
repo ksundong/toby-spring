5ab0f2a6088f1b8b05fd62616ad88715c8e84605
IdionKim <ksundong@gmail.com>
2020-08-17 10:18:53 +0900

    test: countUsers, getCount 테스트 코드 작성

--- src/test/java/dev/idion/springbook/user/dao/UserDaoTest.java
+++ src/test/java/dev/idion/springbook/user/dao/UserDaoTest.java
@@ -29,4 +29,26 @@ class UserDaoTest {
     assertEquals(user1.getName(), user2.getName());
     assertEquals(user1.getPassword(), user2.getPassword());
   }
+
+  @Test
+  void count() throws SQLException {
+    ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+    UserService userService = context.getBean(UserService.class);
+
+    User user1 = new User("gyumee", "박성철", "springno1");
+    User user2 = new User("leegw700", "이길원", "springno2");
+    User user3 = new User("bumjin", "박범진", "springno3");
+
+    userService.deleteUsers();
+    assertEquals(0, userService.countUsers());
+
+    userService.addUser(user1);
+    assertEquals(1, userService.countUsers());
+
+    userService.addUser(user2);
+    assertEquals(2, userService.countUsers());
+
+    userService.addUser(user3);
+    assertEquals(3, userService.countUsers());
+  }
 }
