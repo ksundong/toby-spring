78ea0c6d4e05e4c0481b956a5d2bcb0c2852f4c3
IdionKim <ksundong@gmail.com>
2020-08-18 11:30:42 +0900

    test: 스프링 테스트 애플리케이션 컨텍스트가 정말 공유되는지 확인하는 테스트 작성

---
+++ src/test/java/dev/idion/springbook/learningtest/junit/JUnitConfig.java
@@ -0,0 +1,8 @@
+package dev.idion.springbook.learningtest.junit;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JUnitConfig {
+
+}
--- src/test/java/dev/idion/springbook/learningtest/junit/JUnitTest.java
+++ src/test/java/dev/idion/springbook/learningtest/junit/JUnitTest.java
@@ -5,16 +5,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = JUnitConfig.class)
 class JUnitTest {
 
+  static ApplicationContext contextObject = null;
+
   static Set<JUnitTest> testObjects = new HashSet<>();
+  @Autowired
+  ApplicationContext context;
 
   @Test
   void test1() {
     assertThat(this).isNotIn(testObjects);
     assertThat(testObjects).doesNotContain(this);
     testObjects.add(this);
+
+    assertThat(contextObject == null || contextObject == this.context).isTrue();
   }
 
   @Test
@@ -22,6 +35,10 @@ class JUnitTest {
     assertThat(this).isNotIn(testObjects);
     assertThat(testObjects).doesNotContain(this);
     testObjects.add(this);
+    assertThat(contextObject).satisfiesAnyOf(
+        contextObject -> assertThat(contextObject).as("실패이유").isNull(),
+        contextObject -> assertThat(contextObject).isSameAs(this.context)
+    );
   }
 
   @Test
@@ -29,5 +46,6 @@ class JUnitTest {
     assertThat(this).isNotIn(testObjects);
     assertThat(testObjects).doesNotContain(this);
     testObjects.add(this);
+    assertThat(contextObject == null || contextObject == this.context).isTrue();
   }
 }
