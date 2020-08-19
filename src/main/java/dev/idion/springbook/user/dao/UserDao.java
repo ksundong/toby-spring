187cfadb03e7345dab84d8bdc2ad89c6ffdda0eb
IdionKim <ksundong@gmail.com>
2020-08-19 15:05:18 +0900

    refactor: jdbcTemplate에도 결합이 생기기 때문에 jdbcOperations 인터페이스로 DI되도록 수정

--- src/main/java/dev/idion/springbook/user/dao/UserDao.java
+++ src/main/java/dev/idion/springbook/user/dao/UserDao.java
@@ -2,14 +2,14 @@ package dev.idion.springbook.user.dao;
 
 import dev.idion.springbook.user.domain.User;
 import java.util.List;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserDao {
 
-  private final JdbcTemplate jdbcTemplate;
+  private final JdbcOperations jdbcOperations;
   private final RowMapper<User> userMapper = (rs, rowNum) -> {
     User user = new User();
     user.setId(rs.getString("id"));
@@ -18,29 +18,29 @@ public class UserDao {
     return user;
   };
 
-  public UserDao(JdbcTemplate jdbcTemplate) {
-    this.jdbcTemplate = jdbcTemplate;
+  public UserDao(JdbcOperations jdbcOperations) {
+    this.jdbcOperations = jdbcOperations;
   }
 
   public void add(User user) {
-    jdbcTemplate.update("insert into USER(id, name, password) VALUES (?,?,?)"
+    jdbcOperations.update("insert into USER(id, name, password) VALUES (?,?,?)"
         , user.getId(), user.getName(), user.getPassword());
   }
 
   public User get(String id) {
-    return this.jdbcTemplate.queryForObject("select * from USER where id = ?"
+    return this.jdbcOperations.queryForObject("select * from USER where id = ?"
         , new Object[]{id}, this.userMapper);
   }
 
   public void deleteAll() {
-    this.jdbcTemplate.update("delete from USER");
+    this.jdbcOperations.update("delete from USER");
   }
 
   public Integer getCount() {
-    return this.jdbcTemplate.queryForObject("select count(*) from USER", Integer.class);
+    return this.jdbcOperations.queryForObject("select count(*) from USER", Integer.class);
   }
 
   public List<User> getAll() {
-    return this.jdbcTemplate.query("select * from USER order by id", this.userMapper);
+    return this.jdbcOperations.query("select * from USER order by id", this.userMapper);
   }
 }
