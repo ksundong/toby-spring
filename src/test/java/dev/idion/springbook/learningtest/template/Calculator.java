c40cb38484494a04d0258c203b669334287abcd5
IdionKim <ksundong@gmail.com>
2020-08-19 12:06:24 +0900

    refactor: 템플릿/콜백 패턴으로 리팩토링

--- src/test/java/dev/idion/springbook/learningtest/template/Calculator.java
+++ src/test/java/dev/idion/springbook/learningtest/template/Calculator.java
@@ -7,14 +7,20 @@ import java.io.IOException;
 public class Calculator {
 
   public Integer calcSum(String filepath) throws IOException {
-    int sum;
-    try (BufferedReader br = new BufferedReader(new FileReader(filepath))) {
-      sum = 0;
+    return fileReadTemplate(filepath, br -> {
+      int sum = 0;
       String line;
       while ((line = br.readLine()) != null) {
         sum += Integer.parseInt(line);
       }
       return sum;
+    });
+  }
+
+  public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback)
+      throws IOException {
+    try (BufferedReader br = new BufferedReader(new FileReader(filepath))) {
+      return callback.doSomethingWithReader(br);
     } catch (IOException e) {
       System.out.println(e.getMessage());
       throw e;
