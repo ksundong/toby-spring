43182a0ad933363b0c99da369b64e74f2d0d90c8
IdionKim <ksundong@gmail.com>
2020-08-21 15:16:05 +0900

    refactor: 명확하지 않은 테스트 코드를 명확하게 리팩토링(업그레이드 될것인가?)

--- src/test/java/dev/idion/springbook/user/service/UserServiceTest.java
+++ src/test/java/dev/idion/springbook/user/service/UserServiceTest.java
@@ -51,11 +51,11 @@ class UserServiceTest {
     }
 
     userService.upgradeLevels();
-    checkLevel(users.get(0), Level.BASIC);
-    checkLevel(users.get(1), Level.SILVER);
-    checkLevel(users.get(2), Level.SILVER);
-    checkLevel(users.get(3), Level.GOLD);
-    checkLevel(users.get(4), Level.GOLD);
+    checkLevelUpgraded(users.get(0), false);
+    checkLevelUpgraded(users.get(1), true);
+    checkLevelUpgraded(users.get(2), false);
+    checkLevelUpgraded(users.get(3), true);
+    checkLevelUpgraded(users.get(4), false);
   }
 
   @Test
@@ -76,8 +76,12 @@ class UserServiceTest {
     assertThat(userWithoutLevelRead).isEqualToComparingFieldByField(userWithoutLevel);
   }
 
-  private void checkLevel(User user, Level expectedLevel) {
+  private void checkLevelUpgraded(User user, boolean upgraded) {
     User userUpdate = userDao.get(user.getId());
-    assertThat(userUpdate.getLevel()).isEqualByComparingTo(expectedLevel);
+    if (upgraded) {
+      assertThat(userUpdate.getLevel()).isEqualByComparingTo(user.getLevel().nextLevel());
+    } else {
+      assertThat(userUpdate.getLevel()).isEqualByComparingTo(user.getLevel());
+    }
   }
 }
