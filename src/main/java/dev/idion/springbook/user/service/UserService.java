1f68f9728a80aac36e7d8839add6e0ba9b4bbb08
IdionKim <ksundong@gmail.com>
2020-08-21 13:43:10 +0900

    refactor: 사용하지 않는 메서드 제거 (단순 Dao의 메서드를 호출하기만 하는 메서드)

--- src/main/java/dev/idion/springbook/user/service/UserService.java
+++ src/main/java/dev/idion/springbook/user/service/UserService.java
@@ -1,8 +1,6 @@
 package dev.idion.springbook.user.service;
 
 import dev.idion.springbook.user.dao.UserDao;
-import dev.idion.springbook.user.domain.User;
-import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,30 +10,5 @@ public class UserService {
 
   public UserService(UserDao userDao) {
     this.userDao = userDao;
-  }
-
-  public User getUser(String id) {
-    return userDao.get(id);
-  }
-
-  public void addUser(User user) {
-    userDao.add(user);
-    System.out.println(user.getId() + " 등록 성공");
-  }
-
-  public void deleteUsers() {
-    userDao.deleteAll();
-  }
-
-  public int countUsers() {
-    return userDao.getCount();
-  }
-
-  public List<User> getAll() {
-    return userDao.getAll();
-  }
-
-  public void updateUser(User user) {
-    userDao.update(user);
   }
 }
