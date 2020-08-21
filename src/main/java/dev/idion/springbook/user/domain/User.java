4e7b2936036ae26b571d60f21f8fea62c3e8c2ba
IdionKim <ksundong@gmail.com>
2020-08-21 14:58:13 +0900

    refactor: User가 스스로 레벨이 업그레이드 가능한지 확인해서 업그레이드 하도록 리팩토링

--- src/main/java/dev/idion/springbook/user/domain/User.java
+++ src/main/java/dev/idion/springbook/user/domain/User.java
@@ -22,6 +22,14 @@ public class User {
     this.level = level;
   }
 
+  public void upgradeLevel() {
+    Level nextLevel = this.level.nextLevel();
+    if (nextLevel == null) {
+      throw new IllegalStateException(this.level + "은 업그레이드가 불가능합니다.");
+    }
+    this.level = nextLevel;
+  }
+
   public String getId() {
     return id;
   }
