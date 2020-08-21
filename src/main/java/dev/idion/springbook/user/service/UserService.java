b0a66dec8367def39f84d68c52151465bd62e2b7
IdionKim <ksundong@gmail.com>
2020-08-21 14:02:15 +0900

    feat: 사용자 레벨 상승 비즈니스 로직 구현
    테스트를 할 때 사용되는 데이터가 경계값이 존재하는 경우라면 사용할 데이터를 경계값의 전후로 선택하는 것이 좋다.

--- src/main/java/dev/idion/springbook/user/service/UserService.java
+++ src/main/java/dev/idion/springbook/user/service/UserService.java
@@ -1,6 +1,9 @@
 package dev.idion.springbook.user.service;
 
 import dev.idion.springbook.user.dao.UserDao;
+import dev.idion.springbook.user.domain.Level;
+import dev.idion.springbook.user.domain.User;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,5 +13,25 @@ public class UserService {
 
   public UserService(UserDao userDao) {
     this.userDao = userDao;
+  }
+
+  public void upgradeLevels() {
+    List<User> users = userDao.getAll();
+    for (User user : users) {
+      boolean levelHasChanged;
+      if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
+        user.setLevel(Level.SILVER);
+        levelHasChanged = true;
+      } else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
+        user.setLevel(Level.GOLD);
+        levelHasChanged = true;
+      } else {
+        levelHasChanged = false;
+      }
+
+      if (levelHasChanged) {
+        userDao.update(user);
+      }
+    }
   }
 }
--- src/test/java/dev/idion/springbook/user/service/UserServiceTest.java
+++ src/test/java/dev/idion/springbook/user/service/UserServiceTest.java
@@ -3,6 +3,12 @@ package dev.idion.springbook.user.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import dev.idion.springbook.user.dao.TestDaoFactory;
+import dev.idion.springbook.user.dao.UserDao;
+import dev.idion.springbook.user.domain.Level;
+import dev.idion.springbook.user.domain.User;
+import java.util.List;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +20,46 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 class UserServiceTest {
 
   @Autowired
+  UserDao userDao;
+
+  @Autowired
   UserService userService;
+
+  List<User> users;
+
+  @BeforeEach
+  void setUp() {
+    users = Lists.newArrayList(
+        new User("bumjin", "박범진", "p1", 49, 0, Level.BASIC),
+        new User("joytouch", "강명성", "p2", 50, 0, Level.BASIC),
+        new User("erwins", "신승한", "p3", 60, 29, Level.SILVER),
+        new User("madnite1", "이상호", "p4", 60, 30, Level.SILVER),
+        new User("green", "오민규", "p5", 100, 100, Level.GOLD)
+    );
+  }
 
   @Test
   void bean() {
     assertThat(userService).isNotNull();
+  }
+
+  @Test
+  void upgradeLevels() {
+    userDao.deleteAll();
+    for (User user : users) {
+      userDao.add(user);
+    }
+
+    userService.upgradeLevels();
+    checkLevel(users.get(0), Level.BASIC);
+    checkLevel(users.get(1), Level.SILVER);
+    checkLevel(users.get(2), Level.SILVER);
+    checkLevel(users.get(3), Level.GOLD);
+    checkLevel(users.get(4), Level.GOLD);
+  }
+
+  private void checkLevel(User user, Level expectedLevel) {
+    User userUpdate = userDao.get(user.getId());
+    assertThat(userUpdate.getLevel()).isEqualByComparingTo(expectedLevel);
   }
 }
