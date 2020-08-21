8d846552c0c25c188f48280244de33305b834830
IdionKim <ksundong@gmail.com>
2020-08-21 14:44:34 +0900

    refactor: 사용자 레벨 업그레이드 비즈니스 로직 리팩토링

--- src/main/java/dev/idion/springbook/user/service/UserService.java
+++ src/main/java/dev/idion/springbook/user/service/UserService.java
@@ -18,19 +18,8 @@ public class UserService {
   public void upgradeLevels() {
     List<User> users = userDao.getAll();
     for (User user : users) {
-      boolean levelHasChanged;
-      if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
-        user.setLevel(Level.SILVER);
-        levelHasChanged = true;
-      } else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
-        user.setLevel(Level.GOLD);
-        levelHasChanged = true;
-      } else {
-        levelHasChanged = false;
-      }
-
-      if (levelHasChanged) {
-        userDao.update(user);
+      if (canUpgradeLevel(user)) {
+        upgradeLevel(user);
       }
     }
   }
@@ -40,5 +29,28 @@ public class UserService {
       user.setLevel(Level.BASIC);
     }
     userDao.add(user);
+  }
+
+  private boolean canUpgradeLevel(User user) {
+    Level currentLevel = user.getLevel();
+    switch (currentLevel) {
+      case BASIC:
+        return user.getLogin() >= 50;
+      case SILVER:
+        return user.getRecommend() >= 30;
+      case GOLD:
+        return false;
+      default:
+        throw new IllegalArgumentException("Unknown Level: " + currentLevel);
+    }
+  }
+
+  private void upgradeLevel(User user) {
+    if (user.getLevel() == Level.BASIC) {
+      user.setLevel(Level.SILVER);
+    } else if (user.getLevel() == Level.SILVER) {
+      user.setLevel(Level.GOLD);
+    }
+    userDao.update(user);
   }
 }
