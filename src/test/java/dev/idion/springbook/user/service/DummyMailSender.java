f9562837f09d6b3a2038f2ebd08dc3835dbb7d8b
IdionKim <ksundong@gmail.com>
2020-08-22 15:12:40 +0900

    feat: 메일 정보를 확인하도록 DummyMailSender에 기능 추가

--- src/test/java/dev/idion/springbook/user/service/DummyMailSender.java
+++ src/test/java/dev/idion/springbook/user/service/DummyMailSender.java
@@ -8,9 +8,13 @@ public class DummyMailSender implements MailSender {
 
   @Override
   public void send(SimpleMailMessage simpleMessage) throws MailException {
+    System.out.println(simpleMessage);
   }
 
   @Override
   public void send(SimpleMailMessage... simpleMessages) throws MailException {
+    for (SimpleMailMessage simpleMessage : simpleMessages) {
+      System.out.println(simpleMessage);
+    }
   }
 }
