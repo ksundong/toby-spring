d8aacbeaaf79f0cbfdfb55af3bbb891c4d7df088
IdionKim <ksundong@gmail.com>
2020-08-22 14:53:12 +0900

    feat: 유저 레벨 변경 메일 전송기능 구현

--- src/main/java/dev/idion/springbook/user/service/UserService.java
+++ src/main/java/dev/idion/springbook/user/service/UserService.java
@@ -4,6 +4,13 @@ import dev.idion.springbook.user.dao.UserDao;
 import dev.idion.springbook.user.domain.Level;
 import dev.idion.springbook.user.domain.User;
 import java.util.List;
+import java.util.Properties;
+import javax.mail.Message.RecipientType;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
@@ -56,5 +63,25 @@ public class UserService {
   protected void upgradeLevel(User user) {
     this.userLevelUpgradePolicy.upgradeLevel(user);
     this.userDao.update(user);
+    sendUpgradeEMail(user);
+  }
+
+  private void sendUpgradeEMail(User user) {
+    Properties props = new Properties();
+    props.put("mail.smtp.host", "mail.ksug.org");
+    Session s = Session.getInstance(props, null);
+
+    MimeMessage message = new MimeMessage(s);
+    try {
+      message.setFrom(new InternetAddress("useradmin@ksug.org"));
+      message.addRecipient(RecipientType.TO, new InternetAddress(user.getEmail()));
+      message.setSubject("Upgrade 안내", "UTF-8");
+      message.setText("사용자님의 등급이 " + user.getLevel().name() + "로 업그레이드 되었습니다.",
+          "UTF-8", "html");
+
+      Transport.send(message);
+    } catch (MessagingException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
