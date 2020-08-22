08dfd8c00b6abd52deb40731a8d6ebda71a45a61
IdionKim <ksundong@gmail.com>
2020-08-22 15:08:51 +0900

    test: DummyMailSender를 이용한 테스트 코드 수정

--- src/test/java/dev/idion/springbook/user/dao/TestDaoFactory.java
+++ src/test/java/dev/idion/springbook/user/dao/TestDaoFactory.java
@@ -1,10 +1,17 @@
 package dev.idion.springbook.user.dao;
 
+import dev.idion.springbook.user.service.DummyMailSender;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.mail.MailSender;
+import org.springframework.transaction.PlatformTransactionManager;
 
+@Configuration
 @ComponentScan(basePackages = "dev.idion.springbook.user")
 public class TestDaoFactory {
 
@@ -18,5 +25,20 @@ public class TestDaoFactory {
     dataSource.setPassword("book");
 
     return dataSource;
+  }
+
+  @Bean
+  public JdbcTemplate jdbcTemplate() {
+    return new JdbcTemplate(dataSource());
+  }
+
+  @Bean
+  public PlatformTransactionManager txManager() {
+    return new DataSourceTransactionManager(dataSource());
+  }
+
+  @Bean
+  public MailSender mailSender() {
+    return new DummyMailSender();
   }
 }
