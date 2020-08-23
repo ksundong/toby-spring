6a1d89126d82941e2c98bf693c12c12864a3a94d
IdionKim <ksundong@gmail.com>
2020-08-23 13:52:43 +0900

    test: mockito를 이용한 테스트 코드리팩토링

--- src/test/java/dev/idion/springbook/user/service/UserServiceTest.java
+++ src/test/java/dev/idion/springbook/user/service/UserServiceTest.java
@@ -4,21 +4,23 @@ import static dev.idion.springbook.user.service.UserLevelUpgradeNormalPolicy.MIN
 import static dev.idion.springbook.user.service.UserLevelUpgradeNormalPolicy.MIN_RECOMMEND_FOR_GOLD;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import dev.idion.springbook.user.dao.TestDaoFactory;
 import dev.idion.springbook.user.dao.UserDao;
 import dev.idion.springbook.user.domain.Level;
 import dev.idion.springbook.user.domain.User;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DuplicateKeyException;
-import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.test.context.ContextConfiguration;
@@ -68,29 +70,29 @@ class UserServiceTest {
 
   @Test
   void upgradeLevels() {
-    MockMailSender mockMailSender = new MockMailSender();
-    MockUserDao mockUserDao = new MockUserDao(this.users);
+    UserDao mockUserDao = mock(UserDao.class);
+    when(mockUserDao.getAll()).thenReturn(this.users);
+    MailSender mockMailSender = mock(MailSender.class);
     UserService testUserService = new UserServiceTx(
         new UserServiceImpl(mockUserDao, mockMailSender, this.userLevelUpgradePolicy),
         this.txManager);
 
     testUserService.upgradeLevels();
 
-    List<User> updated = mockUserDao.getUpdated();
-    assertThat(updated).hasSize(2);
-    checkUserAndLevel(updated.get(0), "joytouch", Level.SILVER);
-    checkUserAndLevel(updated.get(1), "madnite1", Level.GOLD);
+    verify(mockUserDao, times(2)).update(any(User.class));
+    verify(mockUserDao).update(users.get(1));
+    assertThat(users.get(1).getLevel()).isEqualByComparingTo(Level.SILVER);
+    verify(mockUserDao).update(users.get(3));
+    assertThat(users.get(3).getLevel()).isEqualByComparingTo(Level.GOLD);
 
-    List<String> requests = mockMailSender.getRequests();
-    assertThat(requests).hasSize(2);
-    assertThat(requests.get(0)).isEqualTo(users.get(1).getEmail());
-    assertThat(requests.get(1)).isEqualTo(users.get(3).getEmail());
+    ArgumentCaptor<SimpleMailMessage> mailMessageArgumentCaptor =
+        ArgumentCaptor.forClass(SimpleMailMessage.class);
+    verify(mockMailSender, times(2)).send(mailMessageArgumentCaptor.capture());
+    List<SimpleMailMessage> mailMessages = mailMessageArgumentCaptor.getAllValues();
+    assertThat(mailMessages.get(0).getTo()[0]).isEqualTo(users.get(1).getEmail());
+    assertThat(mailMessages.get(1).getTo()[0]).isEqualTo(users.get(3).getEmail());
   }
 
-  private void checkUserAndLevel(User user, String expectedId, Level expectedLevel) {
-    assertThat(user.getId()).isEqualTo(expectedId);
-    assertThat(user.getLevel()).isEqualByComparingTo(expectedLevel);
-  }
 
   @Test
   void add() {
@@ -162,70 +164,5 @@ class UserServiceTest {
 
   static class TestUserServiceException extends RuntimeException {
 
-  }
-
-  static class MockMailSender implements MailSender {
-
-    private final List<String> requests = new ArrayList<>();
-
-    public List<String> getRequests() {
-      return requests;
-    }
-
-    @Override
-    public void send(SimpleMailMessage simpleMessage) throws MailException {
-      requests.add(Objects.requireNonNull(simpleMessage.getTo())[0]);
-    }
-
-    @Override
-    public void send(SimpleMailMessage... simpleMessages) throws MailException {
-      for (SimpleMailMessage simpleMessage : simpleMessages) {
-        requests.add(Objects.requireNonNull(simpleMessage.getTo())[0]);
-      }
-    }
-  }
-
-  static class MockUserDao implements UserDao {
-
-    private final List<User> users;
-    private final List<User> updated = new ArrayList<>();
-
-    private MockUserDao(List<User> users) {
-      this.users = users;
-    }
-
-    @Override
-    public List<User> getAll() {
-      return this.users;
-    }
-
-    @Override
-    public void update(User user) {
-      updated.add(user);
-    }
-
-    public List<User> getUpdated() {
-      return updated;
-    }
-
-    @Override
-    public void add(User user) throws DuplicateKeyException {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public User get(String id) {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void deleteAll() {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public Integer getCount() {
-      throw new UnsupportedOperationException();
-    }
   }
 }
