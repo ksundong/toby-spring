4af7f81a5dbb66a2ab0d5df8acdbede59ee488ec
IdionKim <ksundong@gmail.com>
2020-08-24 09:05:42 +0900

    test: 스프링 팩토리 빈 학습테스트 작성

---
+++ src/test/java/dev/idion/springbook/learningtest/spring/factorybean/Message.java
@@ -0,0 +1,18 @@
+package dev.idion.springbook.learningtest.spring.factorybean;
+
+public class Message {
+
+  String text;
+
+  private Message(String text) {
+    this.text = text;
+  }
+
+  public static Message newMessage(String text) {
+    return new Message(text);
+  }
+
+  public String getText() {
+    return text;
+  }
+}
---
+++ src/test/java/dev/idion/springbook/learningtest/spring/factorybean/MessageFactoryBean.java
@@ -0,0 +1,27 @@
+package dev.idion.springbook.learningtest.spring.factorybean;
+
+import org.springframework.beans.factory.FactoryBean;
+
+public class MessageFactoryBean implements FactoryBean<Message> {
+
+  private final String text;
+
+  public MessageFactoryBean(String text) {
+    this.text = text;
+  }
+
+  @Override
+  public Message getObject() throws Exception {
+    return Message.newMessage(this.text);
+  }
+
+  @Override
+  public Class<?> getObjectType() {
+    return Message.class;
+  }
+
+  @Override
+  public boolean isSingleton() {
+    return false;
+  }
+}
