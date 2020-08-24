3e4bd435284fbea75535957eb66e8666de07bdaa
IdionKim <ksundong@gmail.com>
2020-08-24 13:32:40 +0900

    test: 스프링 포인트컷 적용한 프록시 팩토리 빈 테스트

--- src/test/java/dev/idion/springbook/learningtest/jdk/proxy/DynamicProxyTest.java
+++ src/test/java/dev/idion/springbook/learningtest/jdk/proxy/DynamicProxyTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.lang.reflect.Proxy;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 
 class DynamicProxyTest {
 
@@ -38,5 +40,21 @@ class DynamicProxyTest {
     assertThat(proxiedHello.sayHello("Toby")).isEqualTo("HELLO TOBY");
     assertThat(proxiedHello.sayHi("Toby")).isEqualTo("HI TOBY");
     assertThat(proxiedHello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");
+  }
+
+  @Test
+  void pointcutAdvisor() {
+    ProxyFactoryBean pfBean = new ProxyFactoryBean();
+    pfBean.setTarget(new HelloTarget());
+
+    NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+    pointcut.setMappedName("sayH*");
+
+    pfBean.addAdvisor(new DefaultPointcutAdvisor(pointcut, new UppercaseAdvice()));
+
+    Hello proxiedHello = (Hello) pfBean.getObject();
+    assertThat(proxiedHello.sayHello("Toby")).isEqualTo("HELLO TOBY");
+    assertThat(proxiedHello.sayHi("Toby")).isEqualTo("HI TOBY");
+    assertThat(proxiedHello.sayThankYou("Toby")).isEqualTo("Thank You Toby");
   }
 }
