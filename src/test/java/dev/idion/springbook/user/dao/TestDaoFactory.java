8e30ef2d03249735bfb54441811e82daf2dd24d7
IdionKim <ksundong@gmail.com>
2020-08-25 09:46:49 +0900

    refactor: DaoFactory가 ComponentScan 범위에 포함되어 있어서 통과하던 테스트가 있어 수정

--- src/test/java/dev/idion/springbook/user/dao/TestDaoFactory.java
+++ src/test/java/dev/idion/springbook/user/dao/TestDaoFactory.java
@@ -1,10 +1,16 @@
 package dev.idion.springbook.user.dao;
 
 import dev.idion.springbook.user.service.DummyMailSender;
+import dev.idion.springbook.user.service.NameMatchClassMethodPointcut;
+import dev.idion.springbook.user.service.TransactionAdvice;
 import javax.sql.DataSource;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
@@ -12,7 +18,9 @@ import org.springframework.mail.MailSender;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-@ComponentScan(basePackages = "dev.idion.springbook.user")
+@ComponentScan(basePackages = "dev.idion.springbook.user", excludeFilters = {
+    @Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {DaoFactory.class})
+})
 public class TestDaoFactory {
 
   @Bean
@@ -40,5 +48,28 @@ public class TestDaoFactory {
   @Bean
   public MailSender mailSender() {
     return new DummyMailSender();
+  }
+
+  @Bean
+  public TransactionAdvice transactionAdvice() {
+    return new TransactionAdvice(txManager());
+  }
+
+  @Bean
+  public NameMatchClassMethodPointcut transactionPointcut() {
+    NameMatchClassMethodPointcut pointcut = new NameMatchClassMethodPointcut();
+    pointcut.setMappedClassName("*ServiceImpl");
+    pointcut.setMappedName("upgrade*");
+    return pointcut;
+  }
+
+  @Bean
+  public DefaultPointcutAdvisor transactionAdvisor() {
+    return new DefaultPointcutAdvisor(transactionPointcut(), transactionAdvice());
+  }
+
+  @Bean
+  public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+    return new DefaultAdvisorAutoProxyCreator();
   }
 }
