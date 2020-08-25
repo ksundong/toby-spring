5fcfd3ff2922043c16c6768c6324a5a732ec9153
IdionKim <ksundong@gmail.com>
2020-08-25 11:27:10 +0900

    test: 포인트컷 표현식 테스트 헬퍼메서드 추가

--- src/test/java/dev/idion/springbook/learningtest/spring/pointcut/PointcutExpressionTest.java
+++ src/test/java/dev/idion/springbook/learningtest/spring/pointcut/PointcutExpressionTest.java
@@ -29,4 +29,23 @@ class PointcutExpressionTest {
         pointcut.getMethodMatcher().matches(
             Bean.class.getMethod("method"), null)).isFalse();
   }
+
+  private void targetClassPointcutMatches(String expression, boolean... expected) throws Exception {
+    pointcutMatches(expression, expected[0], Target.class, "hello");
+    pointcutMatches(expression, expected[1], Target.class, "hello", String.class);
+    pointcutMatches(expression, expected[2], Target.class, "plus", int.class, int.class);
+    pointcutMatches(expression, expected[3], Target.class, "minus", int.class, int.class);
+    pointcutMatches(expression, expected[4], Target.class, "method");
+    pointcutMatches(expression, expected[5], Bean.class, "method");
+  }
+
+  private void pointcutMatches(String expression, Boolean expected, Class<?> clazz,
+      String methodName, Class<?>... args) throws Exception {
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+    pointcut.setExpression(expression);
+
+    assertThat(pointcut.getClassFilter().matches(clazz)
+        && pointcut.getMethodMatcher().matches(clazz.getMethod(methodName, args), null))
+        .isEqualTo(expected);
+  }
 }
