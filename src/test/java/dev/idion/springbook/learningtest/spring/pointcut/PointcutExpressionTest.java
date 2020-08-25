37d90316e6cfa4cbe425435528ef1b75f681d43e
IdionKim <ksundong@gmail.com>
2020-08-25 11:37:43 +0900

    test: 포인트컷 표현식 테스트 메서드 추가

--- src/test/java/dev/idion/springbook/learningtest/spring/pointcut/PointcutExpressionTest.java
+++ src/test/java/dev/idion/springbook/learningtest/spring/pointcut/PointcutExpressionTest.java
@@ -30,6 +30,30 @@ class PointcutExpressionTest {
             Bean.class.getMethod("method"), null)).isFalse();
   }
 
+  @Test
+  void pointcut() throws Exception {
+    targetClassPointcutMatches("execution(* *(..))", true, true, true, true, true, true);
+    targetClassPointcutMatches("execution(* hello(..))", true, true, false, false, false, false);
+    targetClassPointcutMatches("execution(* hello())", true, false, false, false, false, false);
+    targetClassPointcutMatches("execution(* hello(String))", false, true, false, false, false, false);
+    targetClassPointcutMatches("execution(* meth*(..))", false, false, false, false, true, true);
+    targetClassPointcutMatches("execution(* *(int, int))", false, false, true, true, false, false);
+    targetClassPointcutMatches("execution(* *())", true, false, false, false, true, true);
+    targetClassPointcutMatches("execution(* dev.idion.springbook.learningtest.spring.pointcut.Target.*(..))", true, true, true, true, true, false);
+    targetClassPointcutMatches("execution(* dev.idion.springbook.learningtest.spring.pointcut.*.*(..))", true, true, true, true, true, true);
+    targetClassPointcutMatches("execution(* dev.idion.springbook.learningtest.spring.pointcut..*.*(..))", true, true, true, true, true, true);
+    targetClassPointcutMatches("execution(* dev.idion.springbook..*.*(..))", true, true, true, true, true, true);
+    targetClassPointcutMatches("execution(* com..*.*(..))", false, false, false, false, false, false);
+    targetClassPointcutMatches("execution(* *..Target.*(..))", true, true, true, true, true, false);
+    targetClassPointcutMatches("execution(* *..Tar*.*(..))", true, true, true, true, true, false);
+    targetClassPointcutMatches("execution(* *..*get.*(..))", true, true, true, true, true, false);
+    targetClassPointcutMatches("execution(* *..B*.*(..))", false, false, false, false, false, true);
+    targetClassPointcutMatches("execution(* *..TargetInterface.*(..))", true, true, true, true, false, false);
+    targetClassPointcutMatches("execution(* *(..) throws Runtime*)", false, false, false, true, false, true);
+    targetClassPointcutMatches("execution(int *(..))", false, false, true, true, false, false);
+    targetClassPointcutMatches("execution(void *(..))", true, true, false, false, true, true);
+  }
+
   private void targetClassPointcutMatches(String expression, boolean... expected) throws Exception {
     pointcutMatches(expression, expected[0], Target.class, "hello");
     pointcutMatches(expression, expected[1], Target.class, "hello", String.class);
