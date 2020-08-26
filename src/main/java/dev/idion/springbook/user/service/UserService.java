831cc9e2ebe9ba48efc5d1b38aa2878c38ef7c4e
IdionKim <ksundong@gmail.com>
2020-08-26 12:21:52 +0900

    refactor: 모든 User 관련 데이터 조작이 트랜잭션 경계인 UserService를 통해 진행되도록 변경

--- src/main/java/dev/idion/springbook/user/service/UserService.java
+++ src/main/java/dev/idion/springbook/user/service/UserService.java
@@ -1,10 +1,19 @@
 package dev.idion.springbook.user.service;
 
 import dev.idion.springbook.user.domain.User;
+import java.util.List;
 
 public interface UserService {
 
-  void upgradeLevels();
-
   void add(User user);
+
+  User get(String id);
+
+  List<User> getAll();
+
+  void deleteAll();
+
+  void update(User user);
+
+  void upgradeLevels();
 }
--- src/main/java/dev/idion/springbook/user/service/UserServiceImpl.java
+++ src/main/java/dev/idion/springbook/user/service/UserServiceImpl.java
@@ -42,6 +42,26 @@ public class UserServiceImpl implements UserService {
     this.userDao.add(user);
   }
 
+  @Override
+  public User get(String id) {
+    return userDao.get(id);
+  }
+
+  @Override
+  public List<User> getAll() {
+    return userDao.getAll();
+  }
+
+  @Override
+  public void deleteAll() {
+    userDao.deleteAll();
+  }
+
+  @Override
+  public void update(User user) {
+    userDao.update(user);
+  }
+
   private boolean canUpgradeLevel(User user) {
     return this.userLevelUpgradePolicy.canUpgradeLevel(user);
   }
