14dc9008e7a1563c45fee2eca07938e18e162633
IdionKim <ksundong@gmail.com>
2020-08-26 13:57:37 +0900

    Revert "refactor: 필요없는 애노테이션 제거"
    This reverts commit 21f13aa3

--- src/main/java/dev/idion/springbook/user/dao/DaoFactory.java
+++ src/main/java/dev/idion/springbook/user/dao/DaoFactory.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
@@ -13,6 +14,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @ComponentScan(basePackages = "dev.idion.springbook.user")
+@EnableAspectJAutoProxy
 public class DaoFactory {
 
   @Bean
